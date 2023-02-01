485ee299f953b71124cc242935aa2b2eb243a14f
shankars99 <shankars0899@gmail.com>
2023-02-01 01:20:30 -0500

    RSA based encryption and decryption

---
+++ stork_crypto.py
@@ -0,0 +1,14 @@
+from helper import *
+
+
+def encrypt_message(message_string_object: BinaryString = message, e: int = key.e, n: int = key.n) -> list:
+    data.plain_text = message_string_object.int_array
+    data.encrypted_text = [pow(data, e, n)
+                           for data in message_string_object.int_array]
+
+    return data.encrypted_text
+
+
+def decrypt_message(message_string_object: BinaryString, d=key.d, n=key.n):
+    data.decrypted_text = [pow(char, d, n) for char in data.encrypted_text]
+    return (data.decrypted_text)
